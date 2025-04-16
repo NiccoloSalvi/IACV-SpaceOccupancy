03d4ffa710e82074472b453bcd4b2e4f5ce1f5e4
alessiovilla <alessio3.villa@mail.polimi.it>
2025-04-16 16:23:42 +0200

    Fix CAD measurements

--- Code/StandardLocalization.py
+++ Code/StandardLocalization.py
@@ -16,10 +16,18 @@ frame = cv2.imread("outputFolder/frame_10.png")
 
 # Real world points (x, y, z) in millimeters
 object_points = np.array([
-    [606, 820.6, 132.1],     # LeftLicensPlate 700.9 y
-    [1126, 820.6, 132.1],    # RightLicensPlate
-    [137.5, 791.7, 183.2],   # LeftLight
-    [1594.5, 791.7, 183.2]   # RightLight
+    [606, 820.6, 632.1],     # LeftLicensPlate 700.9 y
+    [1126, 820.6, 632.1],    # RightLicensPlate
+    [137.5, 791.7, 683.2],   # LeftLight
+    [1594.5, 791.7, 683.2]   # RightLight
+], dtype=np.float32)
+
+# Real world points (x, y, z) in millimeters
+new_object_points = np.array([
+    [589.34, 855.35, 129.0],     # LeftLicensPlate 700.9 y
+    [1120.24, 855.35, 129.0],    # RightLicensPlate
+    [188.04, 819.79, 188.18],   # LeftLight
+    [1548.9, 819.79, 188.18]   # RightLight
 ], dtype=np.float32)
 
 
@@ -32,21 +40,35 @@ image_points = np.array([
 ], dtype=np.float32)
 
 # Convert to numpy arrays
-# K IOS API
+# K = IOS API
+
 K = np.array([
-    [2805.4324, 0, 1919.5735],
-    [0, 2805.4324, 1077.1753],
+    [1399.6365, 0, 961.83606],
+    [0, 1399.6365, 538.7572],
     [0, 0, 1]
 ], dtype=np.float32)
 
 # K = OPENCV
-#    [1399.6365, 0, 961.83606],
-#    [0, 1399.6365, 538.7572],
-#    [0, 0, 1]
+"""
+K = np.array([
+    [833.38017686,	0.0,	399.68083124],
+	[0.0,	833.44962868,	550.39137349],
+	[0.0,	0.0,	1.0]
+], dtype=np.float32)
+"""
+
+# K =  API 4K
+"""
+K = np.array([
+    [2805.4324, 0.0, 0.0],
+    [0.0, 2805.4324, 0.0],
+    [1919.5735, 1077.1753, 1.0]
+], dtype=np.float32)
+"""
 
 # Solve for rotation and translation
 success, rvec, tvec = cv2.solvePnP(
-    object_points,
+    new_object_points,
     image_points,
     K,
     distCoeffs=None
@@ -67,9 +89,26 @@ print("Projection P:\n", P)
 points_3D = [
     [0, 0, 0],         # leftBottom
     [1732, 0, 0],      # rightBottom
-    [1732, 1488, 0],   # rightTop
-    [0, 1488, 0]       # leftTop
+    [1732, 1519, 0],   # rightTop
+    [0, 1519, 0]       # leftTop
 ]
+
+points_3Dfront = [
+    [0, 0, -3997],         # leftBottom
+    [1732, 0, -3997],      # rightBottom
+    [1732, 1519, -3997],   # rightTop
+    [0, 1519, -3997]       # leftTop
+]
+
+# Project 3D points to 2D image plane
+projected_pts_front = [project_point(pt, K, R, tvec) for pt in points_3Dfront]
+print("Projected points:", projected_pts_front)
+
+for i in range(4):
+    pt1 = projected_pts_front[i]
+    pt2 = projected_pts_front[(i + 1) % 4]
+    cv2.line(frame, pt1, pt2, (0, 255, 0), 2)
+
 
 # Project 3D points to 2D image plane
 projected_pts = [project_point(pt, K, R, tvec) for pt in points_3D]
